32d1a3ad5150de29b396431b66b154c686c82c17
MuscatRemon <tkrg0202@gmail.com>
2026-01-26 02:08:35 +0900

    firebase初期化、github actionsのworkflow設定

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAbT7TpRaYWGIjq2q9Vqe5jPH-PKsY7SPo",
+  authDomain: "business-card-exchange-91107.firebaseapp.com",
+  projectId: "business-card-exchange-91107",
+  storageBucket: "business-card-exchange-91107.firebasestorage.app",
+  messagingSenderId: "759770131816",
+  appId: "1:759770131816:web:7c78384f81a5f27182fadd",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
