088fae1483c586b2eab31ab9cce4b2a9a371c97c
MuscatRemon <tkrg0202@gmail.com>
2026-01-27 01:11:25 +0900

    テストのsupabaseモック作成

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,6 +1,25 @@
 import { render, screen } from "@testing-library/react";
-import { it, expect, describe } from "vitest";
+import { it, expect, describe, vi } from "vitest";
 import App from "../App";
+
+let mockDB: [] = [
+  // プロジェクトに合った設定をしてください
+  // { id: 1, title: "React", time: 2 },
+  // { id: 2, title: "TEST", time: 4 },
+];
+
+vi.mock("@/utils/supabase", () => {
+  return {
+    default: {
+      from: vi.fn(() => ({
+        select: vi.fn().mockImplementation(async () => ({
+          data: [...mockDB],
+          error: null,
+        })),
+      })),
+    },
+  };
+});
 
 describe("App.tsxテスト", () => {
   it("タイトルが表示されている", () => {
