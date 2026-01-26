f7453b643bd3837953ca3f92677da5df11ac60c6
MuscatRemon <tkrg0202@gmail.com>
2026-01-27 01:01:23 +0900

    chakra-ui導入、supabase導入（接続確認）

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { Button, HStack } from "@chakra-ui/react";
+import supabase from "./utils/supabase";
+
 function App() {
+  const supabaseTest = async () => {
+    const { data, error } = await supabase.from("users").select();
+    if (error) {
+      console.error(error);
+      return;
+    }
+    console.log(data);
+  };
+
+  supabaseTest();
+
   return (
     <>
       <h1>h1タイトル</h1>
+      <HStack>
+        <Button>Click me</Button>
+        <Button>Click me</Button>
+      </HStack>
     </>
   );
 }
